01a8012d80aef35bd26cecd4e9e4140c61782073
arnoldvaz27 <arnoldvaz27.github@gmail.com>
2021-06-06 12:30:40 +0530

    Edit Text Android Project Source Code added to git. This project shows how to create Edit Text in android using java.

---
+++ app/src/main/java/com/example/differentandroidcodes/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.differentandroidcodes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.toString().length() > 0) {
+                    Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "There is no text written in the edit text", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
